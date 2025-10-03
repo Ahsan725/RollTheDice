564896340fc1081f3a971757ee866c8f14a2f402
Ahsan <sbaseer25@gmail.com>
2025-10-03 11:30:25 -0400

    added some more comments

--- src/main/java/com/pluralsight/RollTheDice.java
+++ src/main/java/com/pluralsight/RollTheDice.java
@@ -5,23 +5,24 @@ public class RollTheDice {
         //variables
         int roll1;
         int roll2;
-        //to count 2, 4, 6 and 7
         int count2 = 0, count4 = 0, count6 = 0, count7 = 0;
-
 
         //create an obj of Dice
         Dice dice = new Dice(); //creates a typical 6 sided dice
-        Dice dice2 = new Dice(1, 9); //9 sided dice, won't be using this for this exercise
-
 
         for (var i = 0; i < 100; i++) {
+            //roll the dice twice by calling the roll() method on dice obj
             roll1 = dice.roll();
             roll2 = dice.roll();
+            //accumulate the sum of the two rolls
             var sum = roll1 + roll2;
+            //print what the rolls were
             System.out.println("Roll 1: " + roll1 + ", Roll 2: " + roll2 + ", Sum: " + sum);
+            //create a visual break to see if it matches the values we are interested in
             if (sum == 2 || sum == 4 || sum == 6 || sum == 7) {
                 System.out.println("\nAha! just what I was looking for!\n");
             }
+            //increment the counters if we got a desired number from the sum of the two rolls
             switch (sum) {
                 case 2 -> count2 += 1;
                 case 4 -> count4 += 1;
@@ -29,6 +30,7 @@ public class RollTheDice {
                 case 7 -> count7 += 1;
             }
         }
+        //print results
         System.out.println("The count of 2s is: " + count2);
         System.out.println("The count of 4s is: " + count4);
         System.out.println("The count of 6s is: " + count6);
